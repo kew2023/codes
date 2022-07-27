e22a6a57407a54235a4c6248a1e7548b394db427
kew2023 <58667109+kew2023@users.noreply.github.com>
2022-07-27 15:05:23 +0300

    Update 25_3.py

--- 25_3.py
+++ 25_3.py
@@ -1,3 +1,27 @@
-for i in range(2,1000):
-    if i ** 10 <= 10**12:
-        print(i**9,i**10)
+def f(n):
+    sprime = 0
+    if prime(n): return n
+
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            if prime(i) == True: sprime  += i
+            if prime(n//i) == True: sprime += n//i
+    return sprime
+
+def prime(n):
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+n = 100000+1
+k = 0
+while True:
+    strf = str(f(n))
+    if len(strf) % 2 == 0 and '7' in strf:
+        k += 1
+        print(n,strf)
+    if k == 5:
+        break
+    n += 1
+
